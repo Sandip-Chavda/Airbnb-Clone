4b3ec5215905112c0bd8aaeacdfb27eb4604b639
Sandip-Chavda <chavdas766@gmail.com>
2024-01-13 23:54:29 +0530

    Create and Style Navbar & Footer
    Styling Navbar and Footer with Bootstrap

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlove";
 
@@ -23,6 +24,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 //Home Route
 app.get("/", (req, res) => {

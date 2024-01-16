3556ecc20de622ca8936d9e967b27ac15843962c
Sandip-Chavda <chavdas766@gmail.com>
2024-01-16 22:27:19 +0530

    Review Added
    Review added in show listing page.

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const Review = require("./review");
 // creating listing Schema
 const listingSchema = new Schema({
   title: {
@@ -21,6 +21,12 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 // creating and exporing model for listing schema

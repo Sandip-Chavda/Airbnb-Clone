e6b56503c4660f53b0880f876ecb3d009d65683a
Sandip-Chavda <chavdas766@gmail.com>
2024-01-17 23:15:55 +0530

    Login Page Added
    Login Page Added and style using CSS and Bootstrap.
    
    Login functionality added using Passport npm Package.

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
-//Signup route or signup
+//Signup Route or signup
 router.get("/signup", async (req, res) => {
   res.render("users/signup.ejs");
 });
@@ -23,6 +24,24 @@ router.post(
       res.redirect("/signup");
     }
   })
+);
+
+//Login Route or Login
+router.get("/login", async (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    let { username } = req.body;
+    req.flash("success", `Welcome back ${username}.`);
+    res.redirect("/listings");
+  }
 );
 
 module.exports = router;
